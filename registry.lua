5469c5517868015cf0894d3ba1bdc6bc65615e18
DarThunder <144558873+DarThunder@users.noreply.github.com>
2025-11-23 00:15:55 -0600

    first version of the repository

---
+++ registry.lua
@@ -0,0 +1,27 @@
+return {
+    ["idar-cryptolib"] = {
+        dev = "DarThunder",
+        package_name = "iDar-CryptoLib",
+        latest = "v0.4.0"
+    },
+    ["idar-bignum"] = {
+        dev = "DarThunder",
+        package_name = "iDar-BigNum",
+        latest = "v2.0.0"
+    },
+    ["idar-db"] = {
+        dev = "DarThunder",
+        package_name = "iDar-DB",
+        latest = "v0.1.0-alpha"
+    },
+    ["idar-structures"] = {
+        dev = "DarThunder",
+        package_name = "iDar-Structures",
+        latest = "Beta-v0.1.0"
+    },
+    ["idar-codecs"] = {
+        dev = "DarThunder",
+        package_name = "iDar-Codecs",
+        latest = "Beta-v1.0.2"
+    },
+}
